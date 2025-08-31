d1c3d90dc81e001eac3fa36b5cb47fcf575bef42
Eduardo Vargas <64183666+odinyzeus@users.noreply.github.com>
2025-08-30 23:22:27 -0600

    Application created

---
+++ odoo-main/mexfashion_personalized/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# This module acts as a container for custom modules dependencies only.
+# No logic or models are defined here.
+
+# Import submodules if needed, otherwise leave empty
