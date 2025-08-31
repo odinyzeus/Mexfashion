be5d1909cb549e469f95a5bad8b5dd730559f39e
Eduardo Vargas <64183666+odinyzeus@users.noreply.github.com>
2025-08-31 11:16:01 -0600

    Delete odoo-main/odoo_module directory

--- odoo-main/odoo_module/yuju_factura_cl/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import madkting_config
-from . import sale_order
-from . import res_partner
--- odoo-main/odoo_module/yuju_factura_cl/responses/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = 'Gerardo Alberto Lopez Vega'
-__copyright__ = '(C) 2019 All rights reserved by Madkting'
